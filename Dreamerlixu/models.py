b6fb38237a9fac246e9a5866756346390d29059d
lixu <lixv08@163.com>
2018-10-18 19:29:17 +0800

    use new templates

--- Dreamerlixu/models.py
+++ Dreamerlixu/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -7,13 +8,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
-
-
 
 class Post(models.Model):
     title = models.CharField(max_length=70)
@@ -29,4 +27,4 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:detail', kwargs={'pk': self.pk})
+        return reverse('Dreamerlixu:detail', kwargs={'pk': self.pk})
--- Dreamerlixu/views.py
+++ Dreamerlixu/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
-    return render(request, 'Dreamerlixu/index.html',context={'post_lixt':post_list})
+    return render(request, 'Dreamerlixu/index.html',context={'post_list':post_list})
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'Dreamerlixu/detail.html', context={'post': post})
