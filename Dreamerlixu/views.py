c828a483006eb85dc8d7e649d43c6474513fac7d
lixu <lixv08@163.com>
2018-10-19 13:36:35 +0800

    markdown support

--- Dreamerlixu/views.py
+++ Dreamerlixu/views.py
@@ -1,5 +1,6 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+import markdown
 
 
 def index(request):
@@ -7,4 +8,10 @@ def index(request):
     return render(request, 'Dreamerlixu/index.html',context={'post_list':post_list})
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
     return render(request, 'Dreamerlixu/detail.html', context={'post': post})
