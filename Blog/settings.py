73df6caf8ffd3423dfd937d7a56750ad1d53d0cc
lixu <lixv08@163.com>
2018-10-21 15:05:51 +0800

    service

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-2bs3i#mkv=)9og-@0)(19l$3uye^x#1d2)qd2xf%fa*-0ohe1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost,','.reamerlixu.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
